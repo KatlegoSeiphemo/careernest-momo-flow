5818d0ab013b1d9e71c37eb25bd6ba3186c47c22
Katlego Seiphemo <110339654+KatlegoSeiphemo@users.noreply.github.com>
2025-09-26 21:20:09 +0200

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -98,7 +98,7 @@ const Index = () => {
         <div className="absolute inset-0 bg-gradient-hero opacity-90"></div>
         <div className="absolute inset-0">
           <img 
-            src="https://sdmntprnorthcentralus.oaiusercontent.com/files/00000000-6d78-622f-82c2-048e798d5204/raw?se=2025-09-26T18%3A52%3A24Z&sp=r&sv=2024-08-04&sr=b&scid=b08b1409-90c2-5b49-abcf-26b344a137da&skoid=e9d2f8b1-028a-4cff-8eb1-d0e66fbefcca&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-09-26T17%3A19%3A31Z&ske=2025-09-27T17%3A19%3A31Z&sks=b&skv=2024-08-04&sig=yorG0bJxu5Y2O8DrkEx5HUN1HkInBNN9%2BGfvUwI8Q%2BI%3D"   
+            src="https://sdmntprnorthcentralus.oaiusercontent.com/files/00000000-bd34-622f-8f12-7865fbb9e0bc/raw?se=2025-09-26T19%3A53%3A34Z&sp=r&sv=2024-08-04&sr=b&scid=53f9408f-1639-5b61-9bbe-0251636382b2&skoid=e9d2f8b1-028a-4cff-8eb1-d0e66fbefcca&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-09-26T17%3A21%3A34Z&ske=2025-09-27T17%3A21%3A34Z&sks=b&skv=2024-08-04&sig=s8Ft11l3cllBye5FanUytfBiO8rduEaSKmqLMWFIG0U%3D"   
             alt="African professionals using CareerNest platform"
             className="w-full h-full object-cover"
           />
